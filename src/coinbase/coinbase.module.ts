3f8964b296c8281192c55b0ed0c26db8a72461e8
Willy PM <w.petitmaire@gmail.com>
2022-03-24 19:15:46 +0100

    Creation structure base accordeon recap

--- src/coinbase/coinbase.module.ts
+++ src/coinbase/coinbase.module.ts
@@ -1,6 +1,10 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RecapituatifComponent} from "./recapituatif/recapituatif.component";
+import {MatNativeDateModule} from "@angular/material/core";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+import {CdkAccordionModule} from "@angular/cdk/accordion";
 
 @NgModule({
   declarations: [
@@ -10,7 +14,12 @@ import {RecapituatifComponent} from "./recapituatif/recapituatif.component";
     RecapituatifComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    HttpClientModule,
+    MatNativeDateModule,
+    ReactiveFormsModule,
+    CdkAccordionModule,
   ]
 })
 export class CoinbaseModule {
--- src/coinbase/recapituatif/recapituatif.component.ts
+++ src/coinbase/recapituatif/recapituatif.component.ts
@@ -9,6 +9,8 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./recapituatif.component.scss']
 })
 export class RecapituatifComponent implements OnInit {
+  items = ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5'];
+  expandedIndex = 0;
 
   constructor() {
   }
